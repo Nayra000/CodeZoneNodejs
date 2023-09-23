55ea0a8ceb3f6eeed12828e25422c7798e90df05
Nayra000 <youssefnayra26@gmail.com>
2023-09-23 22:08:59 +0300

    session 7 part 1(JSend , pagination ,DotEnv )

--- controller/courses-controllers.js
+++ controller/courses-controllers.js
@@ -1,3 +1,4 @@
+const {SUCCESS ,FAIL ,ERROR} =require('../utils/httpStatus')
 
 const {validationResult, body} =require('express-validator')
 let Course =require('../schema/course.model.js')
@@ -5,46 +6,106 @@ let Course =require('../schema/course.model.js')
 
 
 const getAllCourses = async (req, res) => {
-    const courses =await Course.find()
-    res.json(courses);
+    const query = req.query;
+    const limit =query.limit ||3 ;
+    const page =query.page ||1 ;  
+    //Note I don't pass the limit and page in the url and not make default values it will raise an error
+    const skip =(page -1) *limit ;
+    try{
+    const courses =await Course.find({} , {"__v" :false}).limit(limit).skip(skip);
+    res.status(200).json({
+        "status": SUCCESS,
+        "data":{courses} //equavelant to  {"courses": courses }
+    });
+    }
+    catch(err){
+        return res.status(400).json({
+            "status" :ERROR ,
+            "message" :err.message
+        })
+
+    }   
 };
 
 const getCourse =async (req ,res)=>{
     let id = req.params.id;
+    try{
     let course = await Course.findById(id);
     if(course){
-        res.json(course);
+        res.status(200).json({
+            "status": SUCCESS,
+            "data":{course} //equavelant to  {"courses": courses }
+        });
     }
     else{
-        res.status(404).json({msg :"not found"});
+        res.status(404).json({"status": FAIL ,"data":null});
+    }
+    }
+    catch(err){
+        return res.status(400).json({
+            "status" :ERROR,
+            "message" :err.message
+        })
     }
 
 };
 
 const updateCourse =async (req, res)=>{
     let id = req.params.id;
+    try{
     let course =await Course.findByIdAndUpdate(id,{$set : {...req.body}});
     if(!course){
-        return res.status(404).json({msg :"not found"});
+        return res.status(404).json({"status": FAIL ,"data":null});
     }
-    
-    res.status(201).json(course);
-
-};
+    res.status(201).json({
+        "status": SUCCESS,
+        "data":{course} //equavelant to  {"courses": courses }
+    });}
+    catch(err){
+        return res.status(400).json({
+            "status" :ERROR,
+            "message" :err.message
+        })};
+}
 
 const addNewCourse =async (req ,res)=>{    
     const errors =validationResult(req)
     if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+        return res.status(400).json({
+            status:FAIL ,
+            data :errors.array()
+        });
     }
+    try{
     let newCourse = new Course({...req.body});
     await newCourse.save();
-    res.status(201).json(newCourse);
+    res.status(201).json({
+        status :SUCCESS ,
+        data:newCourse
+    });
+    }
+    catch(err){
+        return res.status(400).json({
+            "status" :ERROR,
+            "message" :err.message
+        })
+    }
 };
 
 const deleteCourse = async(req ,res)=>{
+    try{
     await Course.deleteOne({_id :req.params.id});
-    res.status(200).json({"msg" :"deleted"})
+    res.status(200).json({
+        "status": SUCCESS,
+        "data":null
+    })
+    }
+    catch(err){
+        return res.status(400).json({
+            "status" :ERROR,
+            "message" :err.message
+        })
+    }
    
 };
 
--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 
 const mongoose = require('mongoose');
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const coursesRouter =require('./Router/courses.route');
+const {ERROR} =require('./utils/httpStatus')
 
 
 async function main(){
-    await mongoose.connect('mongodb+srv://Nayra:nodejs123@cluster0.5eou9di.mongodb.net/code-zone?retryWrites=true&w=majority&appName=AtlasApp')
+    await mongoose.connect(process.env.MONGO_URL)
 }
 main();
+
 app.use(express.json())
 app.use('/api/courses' ,coursesRouter);
+app.all("*" ,(req ,res) =>{ // It defines a route that matches all http methodes and pathes
+    res.status(400).json({
+        status: ERROR,
+        message: "These Resource is not avaliable"
+    })
+})
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000");
+app.listen(process.env.PORT , () => {
+    console.log("Listening to port ",process.env.PORT);
 });
 
 
